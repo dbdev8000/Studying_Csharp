4ab87935153dc9555d6d59196df29d2df01b043d
dbdev8000 <dbdev8000@gmail.com>
2025-02-02 20:17:21 -0300

    Lesson02

---
+++ Lesson02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write(args.GetValue(0));
+        }
+    }
+}
+
