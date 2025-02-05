43edf168b07ff71bfb7f26469e028d9bd8b6bc67
dbdev8000 <dbdev8000@gmail.com>
2025-02-05 15:18:01 -0300

    Lesson07

--- Lesson02/obj/Debug/net9.0/Lesson02.AssemblyInfo.cs
+++ Lesson02/obj/Debug/net9.0/Lesson02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe3f2943a8221a001aaa80e4f5e324fcb803ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81fc9cd07393ae169e3abeeb0baaa5fa072f5a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson07/Lesson07.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Lesson07{
+    static void Main(){
+        const string nickname = "dbdev8000";
+        const double pi = 3.1415;
+        const string time_now = "3:16pm";
+
+        Console.WriteLine("Nickname: {0}\nPi: {1}\nTime now: {2}", nickname, pi, time_now);
+    }
+}
